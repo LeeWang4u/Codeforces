5fb7d3b91ae6f6d5a6360e93048bb64a0cbd44cc
LeeWang4u <102460287+LeeWang4u@users.noreply.github.com>
2022-06-27 02:08:06 +0700

    Add files via upload

---
+++ Watermelon.cpp
@@ -0,0 +1,21 @@
+//								Create by LeeWang
+//  https://codeforces.com/contest/4/problem/A
+
+#include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+	int k;
+	cin >> k;
+	if (k > 2 && k % 2 == 0)
+	{
+		cout << "YES";
+	}
+	else cout << "NO";
+
+}
