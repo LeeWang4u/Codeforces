d0aea69f16f5be40129afed16bb9195433c20893
LeeWang4u <102460287+LeeWang4u@users.noreply.github.com>
2022-06-27 09:19:11 +0700

    Easy

---
+++ Domino_piling.cpp
@@ -0,0 +1,13 @@
+//								Create by LeeWang
+//  https://codeforces.com/contest/50/problem/A
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a * b / 2;
+}
